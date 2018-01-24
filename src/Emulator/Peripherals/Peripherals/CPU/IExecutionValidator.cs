64d911a2b4dd873ae561196db92f3fac5506089f
Eli Boling <eli@dovermicrosystems.com>
2018-01-24 09:01:05 -0500

    Added copyright notices.

--- src/Emulator/Peripherals/Peripherals/CPU/IExecutionValidator.cs
+++ src/Emulator/Peripherals/Peripherals/CPU/IExecutionValidator.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2017-2018 Dover Microsystems, Inc.  All rights reserved.
+// Use and disclosure subject to license. No claim made to open source code or materials.
+
 using System;
 
 namespace Antmicro.Renode.Peripherals.CPU
